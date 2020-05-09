2b51420d14b4401753d99c206c20cd83bf00de44
MoshiNAT <kommanat02@gmail.com>
2020-05-09 19:23:25 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const app = express()
 const port = process.env.PORT || 4000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/kreangsak', (req, res) => {
     res.send('Hello GET')
