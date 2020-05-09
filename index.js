609c740c0a0b3b8d48dd292d5182761d66b46003
MoshiNAT <kommanat02@gmail.com>
2020-05-09 19:30:02 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const app = express()
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,5 +10,8 @@ app.get('/kreangsak', (req, res) => {
     res.send('Hello GET')
     });
 app.post('/kreangsak',async (req, res) => {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
     res.sendStatus(200)
     });
