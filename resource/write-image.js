652c0fc7402c86f76c61efe2f6efd53c9dd0d7ab
MoshiNAT <kommanat02@gmail.com>
2020-05-11 18:13:26 +0700

    write image

--- resource/write-image.js
+++ resource/write-image.js
@@ -4,7 +4,7 @@ const request = require('request')
 const {Storage} = require('@google-cloud/storage');
 
 const image = (id_image,reply_token,userId,myCache) =>{
-
+    console.log(id_image)
     const LINE_HEADER = {
         "Content-Type": "application/json",
         "Authorization": "Bearer tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU="
@@ -16,7 +16,7 @@ const image = (id_image,reply_token,userId,myCache) =>{
         encoding: null // แก้ปัญหา binary ไม่สมบูรณ์จาก default encoding ที่เป็น utf-8
     }, function(error, response, body) {
 
-        var base64data = Buffer.from(body, 'binary').toString('base64');
+        //var base64data = Buffer.from(body, 'binary').toString('base64');
         console.log("image :",body)
         fs.writeFile("o.jpeg", body, "binary", function (err) {
         console.log(err); // writes out file without error, but it's not a valid image
