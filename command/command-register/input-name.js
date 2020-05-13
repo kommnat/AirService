fc6060a58153268983b102a9d46beec2754c3a5e
MoshiNAT <kommanat02@gmail.com>
2020-05-14 02:13:11 +0700

    add number customer

--- command/command-register/input-name.js
+++ command/command-register/input-name.js
@@ -9,7 +9,7 @@ let input_name = (msg,reply_token,userId,myCache) => {
     
         myCache.set("name_customer"+userId,msg,300000);
         myCache.set("input_tel"+userId,msg,300000);
-        console.log("input_tel :",myCache.set("input_tel"+userId))
+        console.log("input_tel :",myCache.get("input_tel"+userId))
         console.log("name_customer :",msg)
         input(reply_token,msg)  
         myCache.del("input_name_customer"+userId)
